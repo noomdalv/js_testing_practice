1d720d6ef4094dcdcbc836a87d8c3b4c84f52b37
Marcos Medeiros <mvsmdrs@protonmail.com>
2020-02-03 18:55:18 -0300

    Add calculator, string, array and cypher functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,1 +1,60 @@
-module.exports = functions;
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const reverse = (string) => {
+  return string.split('').reverse.join('');
+};
+
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+const arrayAnalysis = (array) => {
+  return { average: array.reduce((prev, cur) => prev += cur)/array.length, min: Math.min(array), max: Math.max(array), length: array.length }
+};
+
+const cypher = (string, shift) => {
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  let encrypt = string.split('');
+  
+  for (let i = 0; i < encrypt.length; i++) {
+    
+    if (alphabet.indexOf(encrypt[i] === -1)) {
+      if (encrypt[i].match(/[A-Z]/)) {
+
+        encrypt[i] = encrypt[i].toLowerCase();
+
+        let replace = alphabet[alphabet.indexOf(encrypt[i]) + shift];
+        encrypt[i] = replace.toUpperCase();
+
+        continue;
+
+      } else {
+
+        continue;
+      }
+    } else {
+      let replace = alphabet[alphabet.indexOf(encrypt[i]) + shift];
+      encrypt[i] = replace;
+    }
+  };
+  
+  return encrypt.join('');
+};
+
+console.log(cypher('abc', 1))
+  
--- functions/functions.test.js
+++ functions/functions.test.js
@@ -1,59 +1,56 @@
 const functions = require('./functions');
 
-// STRING TESTS //
+test('String functions', () => {
+  it('takes a string and returns that string with the first character capitalized', () => {
+    expect(functions.capitalize('hello')).toBe('Hello');
+    expect(functions.capitalize('hello')).toEqual('Hello');
+  });
 
-test('takes a string and returns that string with the first character capitalized', () => {
-  expect(functions.capitalize("hello")).toBe("Hello");
-	expect(functions.capitalize("hello")).toEqual("Hello");
+  it('takes a string and returns it reversed', () => {
+    expect(functions.reverse('hello')).toBe('olleh');
+    expect(functions.reverse('hello')).toEqual('olleh');
+  });
 });
 
-test('takes a string and returns it reversed', () => {
-  expect(functions.reverse("hello")).toBe("olleh");
-	expect(functions.reverse("hello")).toEqual("olleh");
+
+test('Calculator functions', () => {
+  it('add 2 numbers properly', () => {
+    expect(functions.sum(2, 2)).toBe(4);
+    expect(functions.sum(1, 3)).not.toBe(3);
+  });
+
+  it('subtract 2 numbers properly', () => {
+    expect(functions.subtract(2, 2)).toBe(0);
+    expect(functions.subtract(5, 2)).toEqual(-3);
+  });
+
+  it('multiplies 2 numbers properly', () => {
+    expect(functions.multiply(3, 4)).toBe(12);
+    expect(functions.multiply(10, 10)).toEqual(100);
+  });
+
+  it('divides 2 numbers properly', () => {
+    expect(functions.divide(4, 4)).toBe(1);
+    expect(functions.divide(10, 5)).toEqual(2);
+  });
 });
 
-// CALCULATOR TESTS//
 
-test('add 2 numbers properly', () => {
-  expect(functions.sum(2, 2)).toBe(4);
-	expect(functions.sum(1, 3)).not.toBe(3);
+test('Caesar Cypher function', () => {
+  it('converts given string and shift input using caesar cypher', () => {
+    expect(functions.cypher('hello world', 5).toEqual('mjqqt btwqi'));
+  });
+
+  it('converts given string with uppercase letters properly using cypher', () => {
+    expect(functions.cypher('Microverse Javascript', 13).toEqual('Zvpebirefr Wninfpevcg'));
+  });
 });
 
-test('subtract 2 numbers properly', () => {
-	expect(functions.subtract(2, 2)).toBe(0);
-	expect(functions.subtract(5, 2)).toEqual(-3);
-});
 
-test('multiplies 2 numbers properly', () => {
-  expect(functions.multiply(3, 4)).toBe(12);
-	expect(functions.multiply(10, 10)).toEqual(100);
-});
-
-test('divides 2 numbers properly', () => {
-  expect(functions.divide(4, 4)).toBe(1);
-	expect(functions.divide(10, 5)).toEqual(2);
-});
-
-// CYPHER TESTS //
-
-test('converts given string and shift input using caesar cypher', () => {
-	expect(functions.cypher("hello world", 5).toEqual("mjqqt btwqi"));
-
-});
-
-test('converts given string with uppercase letters properly using cypher', () => {
-	expect(functions.cypher("Microverse Javascript", 13).toEqual("Zvpebirefr Wninfpevcg"));
-});
-
-// ARRAY ANALYSIS TESTS //
-
-test('takes an array of numbers and returns object with average, min, max, and length properties', () => {
-	expect(functions.arrayAnalysis([1, 2, 3].).toEqual(
-		{
-		  average: 2,
-		  min: 1,
-		  max: 3,
-		  length: 3
-		};
-	))
+test('Array analysis function', () => {
+  it('takes an array of numbers and returns object with average, min, max, and length properties', () => {
+    expect(functions.arrayAnalysis([1, 2, 3]).toEqual({
+      average: 2, min: 1, max: 3, length: 3,
+    }));
+  });
 });
